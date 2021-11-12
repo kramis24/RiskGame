9cfa2abd7b13de83f501cac022f5476643e72339
phin2 <nguyephi24@up.edu>
2021-11-11 17:38:22 -0800

    unit test

--- app/src/test/java/com/example/riskgame/Risk/infoMessage/RiskGameStateTest.java
+++ app/src/test/java/com/example/riskgame/Risk/infoMessage/RiskGameStateTest.java
@@ -2,6 +2,8 @@ package com.example.riskgame.Risk.infoMessage;
 
 import static org.junit.Assert.*;
 
+import com.example.riskgame.R;
+
 import org.junit.Test;
 
 public class RiskGameStateTest {
@@ -9,12 +11,18 @@ public class RiskGameStateTest {
     @Test
     public void attack() {
         RiskGameState gameState = new RiskGameState();
+        //checks that the attack method returns false when both territories have the same owner
         gameState.getTerritories().get(0).setOwner(0);
         gameState.getTerritories().get(1).setOwner(0);
         assertFalse(gameState.attack(gameState.getTerritories().get(0),gameState.getTerritories().get(1)));
+        //checks that the attack method returns true when territories are owned by different players
         gameState.getTerritories().get(1).setOwner(1);
         gameState.getTerritories().get(0).setTroops(10);
         assertTrue(gameState.attack(gameState.getTerritories().get(0),gameState.getTerritories().get(1)));
+        assertFalse(gameState.attack(gameState.getTerritories().get(1),gameState.getTerritories().get(0)));
+        //checks that the attack method returns false when both territories are owned by the enemy
+        gameState.getTerritories().get(0).setOwner(1);
+        assertFalse((gameState.attack(gameState.getTerritories().get(0),gameState.getTerritories().get(1))));
 
     }
 
@@ -46,5 +54,27 @@ public class RiskGameStateTest {
         gameState.exchangeCards();
         assertTrue(gameState.getTotalTroops() == 4);
         assertTrue(gameState.getCards().get(0).size() == 0);
+    }
+
+    @Test
+    public void copy(){
+        RiskGameState gameState = new RiskGameState();
+        RiskGameState copy = new RiskGameState(gameState);
+        assertTrue(gameState.getCurrentPhase() == copy.getCurrentPhase());
+        assertTrue(gameState.getCurrentTurn() == copy.getCurrentTurn());
+        assertTrue(gameState.getTotalTroops() == copy.getTotalTroops());
+
+        for(int i = 0; i < 41; i++) {
+            assertTrue(gameState.getTerritories().get(i).getName() == copy.getTerritories().get(i).getName());
+            assertTrue(gameState.getTerritories().get(i).getTroops() == copy.getTerritories().get(i).getTroops());
+            assertTrue(gameState.getTerritories().get(i).getOwner() == copy.getTerritories().get(i).getOwner());
+            assertTrue(gameState.getTerritories().get(i).getContinent() == copy.getTerritories().get(i).getContinent());
+        }
+
+        for(int i = 0; i < gameState.getCards().size(); i++) {
+            for(int j = 0; j < gameState.getCards().get(i).size(); j++) {
+                assertTrue(gameState.getCards().get(i).get(j) == copy.getCards().get(i).get(j) );
+            }
+        }
     }
 }
