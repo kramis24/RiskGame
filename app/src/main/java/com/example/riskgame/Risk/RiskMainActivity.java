79c0332f99c96bc285b815fafbd55c95231a752c
phin2 <nguyephi24@up.edu>
2021-11-17 12:38:02 -0800

    Added ability to have multiple players

--- app/src/main/java/com/example/riskgame/Risk/RiskMainActivity.java
+++ app/src/main/java/com/example/riskgame/Risk/RiskMainActivity.java
@@ -56,7 +56,7 @@ public class RiskMainActivity extends GameMainActivity {
             }
         });
 
-        GameConfig defaultConfig = new GameConfig(playerTypes, 2, 5,
+        GameConfig defaultConfig = new GameConfig(playerTypes, 2, 6,
                 "Risk", PORT_NUMBER);
 
         // default players
